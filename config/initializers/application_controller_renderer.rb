41ac7bb735244d9f5df36d65969005531ce2a09a
pro-soichiro <dr.1987626@gmail.com>
2021-09-16 06:54:34 +0000

    [Update] いいね機能まで

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9dcd28dd4de0c9c48aa3d960bf4a564de86f1f47871374fd62b7981d6ff063ebfd648d75eeb37fe07e0236e77db5ea14a61d81608666db9f3b3b60413c7e9043'
